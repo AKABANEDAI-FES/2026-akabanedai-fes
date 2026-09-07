defaf8dd583cad6f6fe6c5094c7d0fa59e32c6f7
1F10250263 <s1f102502632@iniad.org>
2026-09-07 14:10:23 +0900

    フォルダの作成

--- src/routes/_components/campus-map.tsx
+++ src/routes/_components/campus-map.tsx
@@ -1,9 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import styles from './campus-map.module.css'
+
 export const Route = createFileRoute('/_components/campus-map')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/_components/campus-map"!</div>
+  return <div className={styles.map}>Hello "/_components/campus-map"!</div>
 }
