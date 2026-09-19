0efbf3e01c72a0572f338144503f3ee57843e86d
mst-mkt <mail@keito.dev>
2026-09-20 06:59:08 +0900

    refactor: drop footer stories that no longer vary

--- src/components/footer/footer.stories.tsx
+++ src/components/footer/footer.stories.tsx
@@ -15,15 +15,3 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {}
-
-export const FewSponsors: Story = {
-  args: {
-    sponsors: ['アミカ　赤羽西口店', '株式会社カレッジヤード', '内田商店'],
-  },
-}
-
-export const Mobile: Story = {
-  parameters: {
-    viewport: { defaultViewport: 'mobile1' },
-  },
-}
