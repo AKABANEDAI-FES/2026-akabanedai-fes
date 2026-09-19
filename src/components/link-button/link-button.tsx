7208b544e3e8a4677dbbde6efe1eae06f3f9e58c
mst-mkt <mail@keito.dev>
2026-09-20 08:45:38 +0900

    fix: keep the router disabled prop off the anchor element

--- src/components/link-button/link-button.tsx
+++ src/components/link-button/link-button.tsx
@@ -5,10 +5,14 @@ import { cn } from '../../utils/cn'
 
 import styles from './link-button.module.css'
 
-type LinkButtonBaseProps = ComponentProps<'a'>
+type LinkButtonBaseProps = ComponentProps<'a'> & {
+  disabled?: boolean
+}
 
-const LinkButtonBase: FC<LinkButtonBaseProps> = ({ className, ...anchorProps }) => (
-  <a className={cn(styles.link, className)} {...anchorProps} />
-)
+const LinkButtonBase: FC<LinkButtonBaseProps> = ({
+  className,
+  disabled: _disabled,
+  ...anchorProps
+}) => <a className={cn(styles.link, className)} {...anchorProps} />
 
 export const LinkButton = createLink(LinkButtonBase)
