b8d2a8d6434cb0a252204869150ff23c30c1fa49
mst-mkt <mail@keito.dev>
2026-09-20 02:35:33 +0900

    chore(stylelint): disallow color functions in css modules

--- stylelint.config.ts
+++ stylelint.config.ts
@@ -25,6 +25,18 @@ export default {
       files: ['**/*.module.css'],
       rules: {
         'media-feature-name-disallowed-list': ['width', 'min-width', 'max-width'],
+        'function-disallowed-list': [
+          'rgb',
+          'rgba',
+          'hsl',
+          'hsla',
+          'hwb',
+          'lab',
+          'lch',
+          'oklab',
+          'oklch',
+          'color',
+        ],
         'color-no-hex': true,
         'color-named': 'never',
         'scale-unlimited/declaration-strict-value': [
