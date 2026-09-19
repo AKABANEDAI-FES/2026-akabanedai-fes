c5a2bb05bed7cf74180abe309a429ec311a060d1
mst-mkt <mail@keito.dev>
2026-09-20 06:07:07 +0900

    add: image dimensions to the image-with-tape story

--- src/components/image-with-tape/image-with-tape.stories.tsx
+++ src/components/image-with-tape/image-with-tape.stories.tsx
@@ -16,5 +16,7 @@ export const Default: Story = {
   args: {
     src: 'https://placehold.jp/ababab/ffffff/1600x900.png?text=%E7%94%BB%E5%83%8F',
     alt: 'サンプル画像',
+    width: 1600,
+    height: 900,
   },
 }
