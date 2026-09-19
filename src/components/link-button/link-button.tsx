dd6c9c2f6553d960c99709906fc7bed17c16c668
mst-mkt <mail@keito.dev>
2026-09-20 08:26:00 +0900

    refactor: join class names with cn helper

--- src/components/link-button/link-button.tsx
+++ src/components/link-button/link-button.tsx
@@ -1,12 +1,14 @@
 import { createLink } from '@tanstack/react-router'
 import type { ComponentProps } from 'react'
 
+import { cn } from '../../utils/cn'
+
 import styles from './link-button.module.css'
 
 type LinkButtonBaseProps = ComponentProps<'a'>
 
 const LinkButtonBase = ({ className, ...anchorProps }: LinkButtonBaseProps) => (
-  <a className={[styles.link, className].filter(Boolean).join(' ')} {...anchorProps} />
+  <a className={cn(styles.link, className)} {...anchorProps} />
 )
 
 export const LinkButton = createLink(LinkButtonBase)
