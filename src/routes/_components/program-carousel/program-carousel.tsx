7256524b16c6b25c6ec9ab00157090e20479435c
mst-mkt <mail@keito.dev>
2026-09-20 20:22:43 +0900

    fix: name the carousel section

--- src/routes/_components/program-carousel/program-carousel.tsx
+++ src/routes/_components/program-carousel/program-carousel.tsx
@@ -8,7 +8,7 @@ type ProgramCarouselProps = {
 }
 
 export const ProgramCarousel: FC<ProgramCarouselProps> = ({ programs }) => (
-  <section>
+  <section aria-label="企画紹介">
     <Carousel programs={programs} />
   </section>
 )
