a4d315bb156c46a7435d97716c08ac74a52d9bc7
mst-mkt <mail@keito.dev>
2026-09-25 03:21:20 +0900

    fix: lay out the intro decorations for mobile screens

--- src/routes/_components/intro-background/intro-background.tsx
+++ src/routes/_components/intro-background/intro-background.tsx
@@ -13,7 +13,7 @@ type IntroBackgroundProps = {
 
 export const IntroBackground: FC<IntroBackgroundProps> = ({ children }) => (
   <div className={styles.introBackground}>
-    <div className={styles.decorations} aria-hidden="true">
+    <div className={styles.desktopDecorations} aria-hidden="true">
       <div className={styles.layerBack}>
         <div style={{ top: '6cqw', left: '-3cqw' }}>
           <Stripe length="28cqw" thickness="2cqw" from="secondary" to="primary" />
@@ -90,6 +90,89 @@ export const IntroBackground: FC<IntroBackgroundProps> = ({ children }) => (
         </div>
         <div style={{ top: '146cqw', left: '25cqw' }}>
           <Chevrons variant="outline" size="36cqw" />
+        </div>
+      </div>
+    </div>
+    <div className={styles.mobileDecorations} aria-hidden="true">
+      <div className={styles.layerBack}>
+        <div style={{ top: '9cqw', left: '4cqw' }}>
+          <Stripe length="26cqw" thickness="3cqw" from="secondary" to="primary" />
+        </div>
+        <div style={{ top: '-12cqw', left: '20cqw' }}>
+          <Stripe length="45cqw" thickness="5cqw" from="secondary" to="primary" />
+        </div>
+        <div style={{ top: '12cqw', left: '64cqw' }}>
+          <Stripe length="48cqw" thickness="9cqw" from="secondary" to="accent" />
+        </div>
+        <div style={{ top: '56cqw', left: '64cqw' }}>
+          <Stripe length="41cqw" thickness="7cqw" from="secondary" to="primary" />
+        </div>
+        <div style={{ top: '102cqw', left: '54cqw' }}>
+          <Stripe length="62cqw" thickness="11cqw" from="secondary" to="accent" />
+        </div>
+        <div style={{ top: '170cqw', left: '-13cqw' }}>
+          <Stripe length="40cqw" thickness="5cqw" from="secondary" to="accent" />
+        </div>
+        <div style={{ top: '280cqw', left: '67cqw' }}>
+          <Stripe length="50cqw" thickness="8cqw" from="secondary" to="primary" />
+        </div>
+        <div style={{ top: '258cqw', left: '-35cqw' }}>
+          <Stripe length="40cqw" thickness="4cqw" from="secondary" to="primary" />
+        </div>
+        <div style={{ top: '344cqw', left: '-40cqw' }}>
+          <Stripe length="30cqw" thickness="3cqw" from="secondary" to="accent" />
+        </div>
+        <div style={{ top: '369cqw', left: '8cqw' }}>
+          <Stripe length="45cqw" thickness="6cqw" from="secondary" to="accent" />
+        </div>
+      </div>
+      <div className={styles.layerMiddle}>
+        <div style={{ top: '18cqw', left: '48cqw' }}>
+          <Bolt size="28cqw" />
+        </div>
+        <div style={{ top: '89cqw', left: '30cqw' }}>
+          <Bolt size="33cqw" />
+        </div>
+        <div style={{ top: '146cqw', left: '2cqw' }}>
+          <Arrow size="20cqw" />
+        </div>
+        <div style={{ top: '194cqw', left: '84cqw' }}>
+          <Bolt size="26cqw" />
+        </div>
+        <div style={{ top: '254cqw', left: '8cqw' }}>
+          <Bolt size="26cqw" />
+        </div>
+        <div style={{ top: '224cqw', left: '-43cqw' }}>
+          <Bolt size="26cqw" />
+        </div>
+        <div style={{ top: '292cqw', left: '-53cqw' }}>
+          <Arrow size="16cqw" />
+        </div>
+        <div style={{ top: '382cqw', left: '-35cqw' }}>
+          <Bolt size="30cqw" />
+        </div>
+        <div style={{ top: '414cqw', left: '-83cqw' }}>
+          <Bolt size="26cqw" />
+        </div>
+      </div>
+      <div className={styles.layerFront}>
+        <div style={{ top: '4cqw', left: '5cqw' }}>
+          <Chevrons variant="filled" size="62cqw" />
+        </div>
+        <div style={{ top: '83cqw', left: '-4cqw' }}>
+          <Chevrons variant="outline" size="76cqw" />
+        </div>
+        <div style={{ top: '233cqw', left: '-72cqw' }}>
+          <Chevrons variant="filled" size="55cqw" />
+        </div>
+        <div style={{ top: '358cqw', left: '-95cqw' }}>
+          <Chevrons variant="outline" size="70cqw" />
+        </div>
+        <div style={{ top: '404cqw', left: '-15cqw' }}>
+          <Chevrons variant="outline" size="50cqw" />
+        </div>
+        <div style={{ top: '443cqw', left: '-122cqw' }}>
+          <Chevrons variant="filled" size="55cqw" />
         </div>
       </div>
     </div>
